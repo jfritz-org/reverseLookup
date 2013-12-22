c5e1208e6ee41a142fc7abfa5faad9c13f52cdb2
Robert Palmer <Robert.Palmer@robotniko.de>
2013-12-22 16:20:46 +0100

    - Fixed lookup for Norway (gul.no)

---
+++ src/test/java/de/robotniko/reverseLookup/countries/norway/NorwayGul.java
@@ -0,0 +1,157 @@
+package de.robotniko.reverseLookup.countries.norway;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import de.robotniko.MyTestHelper;
+import de.robotniko.reverseLookup.Helper;
+import de.robotniko.reverseLookup.ReverseLookupService;
+import de.robotniko.reverseLookup.exceptions.ReverseLookupException;
+import de.robotniko.reverseLookup.structs.Person;
+
+public class NorwayGul {
+
+	private static final String SEARCH_SITE_NAME = "gul.no";
+	private static ReverseLookupService service = new ReverseLookupService();
+
+	@BeforeClass
+	public static void setup() throws ReverseLookupException {
+		MyTestHelper.initLogging();
+		service.loadConfig(MyTestHelper.class.getResourceAsStream("/reverselookup.xml"));
+	}
+
+	@After
+	public void after() throws InterruptedException {
+		int sleeptime = 3;
+		System.out.println("\nSleeping for " + sleeptime + " seconds");
+		Thread.sleep(sleeptime * 1000);
+	}
+
+	@Test
+	public void testAksnes() throws ReverseLookupException, ParserConfigurationException, SAXException, IOException {
+		Person expected = new Person();
+		expected.setFirstName("Nils");
+		expected.setLastName("Aksnes & Co AS");
+		expected.setStreet("Sandvenvegen 28");
+		expected.setZipCode("5600");
+		expected.setCity("Norheimsund");
+		
+		Helper.testNumberOnSite(service, "+4756553530", SEARCH_SITE_NAME, expected);
+	}
+
+	@Test
+	public void testGriep() throws ReverseLookupException, ParserConfigurationException, SAXException, IOException {
+		Person expected = null; // no entry
+		
+		Helper.testNumberOnSite(service, "+4773945687", SEARCH_SITE_NAME, expected);
+	}
+
+	@Test
+	public void testHelsebanken() throws ReverseLookupException, ParserConfigurationException, SAXException, IOException {
+		Person expected = new Person();
+		expected.setFirstName("Einar");
+		expected.setLastName("Svarstad");
+		expected.setStreet("Hardangerfjordvegen 650 Helsebanken Legekontor");
+		expected.setZipCode("5610");
+		expected.setCity("Øystese");
+		
+		Helper.testNumberOnSite(service, "+4756553850", SEARCH_SITE_NAME, expected);
+	}
+
+	@Test
+	public void testSkage() throws ReverseLookupException, ParserConfigurationException, SAXException, IOException {
+		Person expected = new Person();
+		expected.setFirstName("Jostein");
+		expected.setLastName("Skage");
+		expected.setStreet("Skagevegen 148");
+		expected.setZipCode("5258");
+		expected.setCity("Blomsterdalen");
+		
+		Helper.testNumberOnSite(service, "+4755226351", SEARCH_SITE_NAME, expected);
+	}
+
+	@Test
+	public void testKraftverk() throws ReverseLookupException, ParserConfigurationException, SAXException, IOException {
+		Person expected = new Person();
+		expected.setFirstName("Kvam");
+		expected.setLastName("Kraftverk AS");
+		expected.setStreet("Kaldestad 40");
+		expected.setZipCode("5600");
+		expected.setCity("Norheimsund");
+		
+		Helper.testNumberOnSite(service, "+4756553300", SEARCH_SITE_NAME, expected);
+	}
+
+	@Test
+	public void testWangensteen() throws ReverseLookupException, ParserConfigurationException, SAXException, IOException {
+		Person expected = null; // no entry
+		
+		Helper.testNumberOnSite(service, "+4798043923", SEARCH_SITE_NAME, expected);
+	}
+
+	@Test
+	public void testFargehandel() throws ReverseLookupException, ParserConfigurationException, SAXException, IOException {
+		Person expected = new Person();
+		expected.setFirstName("Scopus");
+		expected.setLastName("AS");
+		expected.setStreet("");
+		expected.setZipCode("5600");
+		expected.setCity("Norheimsund");
+		
+		Helper.testNumberOnSite(service, "+4756551733", SEARCH_SITE_NAME, expected);
+	}
+
+	@Test
+	public void testPPT() throws ReverseLookupException, ParserConfigurationException, SAXException, IOException {
+		Person expected = new Person();
+		expected.setFirstName("");
+		expected.setLastName("");
+		expected.setStreet("Kalfarveien 20");
+		expected.setZipCode("5018");
+		expected.setCity("Bergen");
+		
+		Helper.testNumberOnSite(service, "+4755562250", SEARCH_SITE_NAME, expected);
+	}
+
+	@Test
+	public void testAdventure() throws ReverseLookupException, ParserConfigurationException, SAXException, IOException {
+		Person expected = new Person();
+		expected.setFirstName("Skodei");
+		expected.setLastName("Elin Bjørnæs");
+		expected.setStreet("Markveien 6");
+		expected.setZipCode("9510");
+		expected.setCity("Alta");
+		
+		Helper.testNumberOnSite(service, "+4778445050", SEARCH_SITE_NAME, expected);
+	}
+
+	@Test
+	public void testKvitbrygga() throws ReverseLookupException, ParserConfigurationException, SAXException, IOException {
+		Person expected = new Person();
+		expected.setFirstName("Kvitbrygga");
+		expected.setLastName("AS");
+		expected.setStreet("Furøyveien 260");
+		expected.setZipCode("8178");
+		expected.setCity("Halsa");
+		
+		Helper.testNumberOnSite(service, "+4775750080", SEARCH_SITE_NAME, expected);
+	}
+
+	@Test
+	public void testSamferdselsdepartementet() throws ReverseLookupException, ParserConfigurationException, SAXException, IOException {
+		Person expected = new Person();
+		expected.setFirstName("Kulturdepartementet");
+		expected.setLastName("Idretts-Og Kulturformål");
+		expected.setStreet("Akersgata 59");
+		expected.setZipCode("0180");
+		expected.setCity("Oslo");
+		
+		Helper.testNumberOnSite(service, "+4722249090", SEARCH_SITE_NAME, expected);
+	}
+}
